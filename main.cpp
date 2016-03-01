faf64a1f30f42c801df9d4c40848e3a8620f93a7
user <justForWork113@gmail.com>
2016-03-02 00:37:30 +0300

    Multiple corrections.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ typedef  ssize_t socklen_t;
 #include <arpa/inet.h>
 #include <stdexcept>
 #include <sys/ioctl.h>
+#include <sys/stat.h>
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR   -1
 int errNum (){ return errno;}
@@ -33,6 +34,7 @@ int errNum (){ return errno;}
 #include <mutex>
 #include <cstdio>
 #include <stdarg.h>
+#include <string.h>
 
 template <typename Error>
 void Throw(const char* format,...){
@@ -40,24 +42,27 @@ void Throw(const char* format,...){
     va_list ap;
     char buff[256];
     va_start(ap, format);
-    vsprintf(buff, format, ap);
+    vsnprintf(buff, 256, format, ap);
     va_end(ap);
 
     throw Error (buff);
 }
 #define ThrowRuntime Throw<std::runtime_error>
 
-FILE* logFile;
+FILE* logFile = nullptr;
 std::mutex logMutex;
 void log (const char* format,...){
-  /*  std::lock_guard<std::mutex> lock(logMutex);
+    if (logFile == nullptr)
+        return;
+
+    std::lock_guard<std::mutex> lock(logMutex);
 
     va_list ap;
     va_start(ap, format);
     vfprintf(logFile, format, ap);
     va_end(ap);
 
-    fflush(logFile);*/
+    fflush(logFile);
 }
 //-----------------------------------------------------------------------------
 #include <sstream>
@@ -85,16 +90,20 @@ int demonize (){
 
     switch(pid) {
     case 0:
+        umask(0);
+        setsid();
+        //chdir("/");//?
+
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+        break;
 
     case -1:
+        exit(EXIT_FAILURE);
         break;
 
     default:
-        setsid();
-
-        close(0);
-        close(1);
-        close(2);
         exit(EXIT_SUCCESS);
         break;
     }
@@ -102,8 +111,8 @@ int demonize (){
     return 0;
 }
 
-int openSocket(const std::string &tcp_ip_v4_addres, unsigned int port ){
-    sockaddr_in server;
+int openSocket(const std::string &host, unsigned int port ){
+
     //Create a socket
     int serverSocket = INVALID_SOCKET;
     if((serverSocket = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
@@ -112,15 +121,25 @@ int openSocket(const std::string &tcp_ip_v4_addres, unsigned int port ){
     log("Socket created.\n");
 
     //Prepare the sockaddr_in structure
+    sockaddr_in server;
+    memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( port );    //ToDo Port here
+    server.sin_port = htons( port );
+
+    unsigned char buf[sizeof(struct in6_addr)];
+    inet_pton(AF_INET, host.c_str(), buf);
+    server.sin_addr.s_addr = *((uint32_t*)buf);
+    //server.sin_addr.s_addr = INADDR_ANY;
 
     //Bind
     if( bind(serverSocket ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
         ThrowRuntime("Bind failed with error code : %d" , errNum());
+    log("Bind done.\n");
 
-    puts("Bind done");
+    if( 0 != listen(serverSocket, 50))
+        ThrowRuntime("Listen failed with error code : %d", errNum());
+    log("Server socket done.\n");
+
     return serverSocket;
 }
 
@@ -129,12 +148,12 @@ void processHttpReq (const std::string &req,int socket){
     auto lines = split (req,'\x0A');
     auto header = split (lines[0],' ');
 
-    if (header[0] == "GET"){
+    if ( strcasecmp (header[0].c_str(), "GET") == 0){
         log("GET req\n");
         auto resName = header[1];
 
-        if (resName == "/")
-            resName = "/index.html";
+        //if (resName == "/")
+        //    resName = "/index.html";
 
         if (resName[0] == '/')
             resName = std::string(&resName[1]);
@@ -154,19 +173,18 @@ void processHttpReq (const std::string &req,int socket){
             std::string header = std::string("HTTP/1.0 200 OK\x0D\x0A") +
                                  "Content-Type: text/html\x0D\x0A" +
                                  "Content-Length: "+ std::to_string(buff.size()) +"\x0D\x0A" +
-                                 "Connection: keep-alive\x0D\x0A" +
                                  "\x0D\x0A";
             write(socket,header.c_str(),header.size());
 
-
             write(socket,buff.data(),buff.size());
-
 
             log("Resource %s is sent.\n",resName.c_str());
         }
         else{
             log("Resource %s is not found.\n",resName.c_str());
-            char respound[] = "HTTP/1.0 404 NOT FOUND\x0D\x0AContent-Type: text/html\x0D\x0A\x0D\x0A";
+            char respound[] = "HTTP/1.0 404 NOT FOUND\x0D\x0A"
+                              "Content-Length: 0\x0D\x0A"
+                              "Content-Type: text/html\x0D\x0A\x0D\x0A";
             write(socket,respound,sizeof(respound));
         }
     }
@@ -177,11 +195,12 @@ void processHttpReq (const std::string &req,int socket){
 
 void processConection (int socket){
 
-   auto threadId = std::this_thread::get_id();
-   int conectionId = ((int*)&threadId)[0];
+    auto threadId = std::this_thread::get_id();
+    unsigned int conectionId = ((unsigned int*)&threadId)[0];
     log("Open connection %i\n",conectionId);
 
     char buffer[10*1024];
+    memset(buffer, 0, sizeof(buffer));
 
     while (true){
         auto size = read (socket,&buffer,10*1024);
@@ -191,6 +210,7 @@ void processConection (int socket){
         log("New reqwest in connection %i \n%s\n\n",conectionId,buffer);
 
         processHttpReq (buffer,socket);
+        break;//ToDo add Keep-Alive
 
     }
 
@@ -223,27 +243,25 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (ip == "" or port == "" or dir == ""){
-        printf("You can't set ip, port or dir");
-        exit(EXIT_FAILURE);
-    }
-
     demonize ();
 
-    logFile = fopen ("log.txt", "wb");
+    logFile = fopen ("/tmp/stepic_final_log.txt", "wb");
     if (logFile == NULL){
-        printf("Can't create log");
+        //printf("Can't create log");
         //exit(EXIT_FAILURE);
+    }
+
+    if (ip == "" or port == "" or dir == ""){
+        log("You can't set ip, port or dir");
+        exit(EXIT_FAILURE);
     }
 
     log("Start server %s : %s in %s \n\n", ip.c_str(), port.c_str(), dir.c_str());
 
     chdir(dir.c_str());
 
-    auto serverSocket = openSocket("", std::stoi(port));
+    auto serverSocket = openSocket(ip, std::stoi(port));
     while (1){
-        if( 0 != listen(serverSocket, 50))
-            ThrowRuntime("Listen failed with error code : %d", errNum());
 
         int newsock = INVALID_SOCKET;
         sockaddr_in  cli_addr;
